d4c07bb59714d906de03251686ac7c50e6a55f37
aluno <leodihl@gmail.com>
2024-03-05 21:23:01 -0300

    Exercicio de Switch Case | Parte 4 | Números 1, 2 e 3.

---
+++ src/main/java/com/mycompany/exercicioswitchcase04/ExercicioSwitchCase04.java
@@ -0,0 +1,28 @@
+package com.mycompany.exercicioswitchcase04;
+
+import java.util.Scanner;
+
+public class ExercicioSwitchCase04 {
+
+    public static void main(String[] args) {
+                
+        Scanner teclado = new Scanner(System.in);
+        
+        int x;
+        
+        x = teclado.nextInt();
+        
+        switch (x) {
+            case 1: System.out.println("Número é 1");
+                if (x < 2) {System.out.println("Número não é maior que 2");} 
+                break;
+            case 2: System.out.println("Número é 2");
+                break; 
+            case 3: System.out.println("Número é 3");
+                if (x > 2) {System.out.println("Número é maior que 2");}
+                break; 
+            default: System.out.println("Número não é 1, 2 ou 3.");
+                break;  
+        }
+    }
+}    
